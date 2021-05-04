91c09c8b24f33c269ba8621c76431d4a3bb19991
Edvard Ghazaryan <edvard@mini.local>
2021-05-03 23:03:49 -0700

    added build shell script

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## check existence of libtorch
+if [ ! -d "ext/libtorch" ] 
+then
+    echo "Downloading libtorch ..."
+    rm -rf ext
+    mkdir ext
+    cd ext
+    wget https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.7.1.zip
+    echo "Extracting libtorch ..."
+    tar xvzf libtorch-macos-1.7.1.zip
+    cd -
+fi
+
+## First time build case
+if [ ! -d "build" ] 
+then
+    echo "Create build directory."
+    mkdir build
+fi
+
+## Configure and build
+## Change dir; configure; build; move back
+cd build; cmake ..; make -j; cd -
