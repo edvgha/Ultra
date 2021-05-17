69a91ed489168bfc5b89a51c8bbcb9cf3467d035
Edvard Ghazaryan <edvard@mini.local>
2021-05-17 02:34:03 -0700

    added comment

--- src/ultra/native_library.h
+++ src/ultra/native_library.h
@@ -5,6 +5,6 @@
 namespace ultra 
 {
 
-    // TODO
+    // Checks if given schema is contains in NativeSchemas library
     bool containsInNativeLibrary(const c10::FunctionSchema& schema);
 } // namespace ultra
