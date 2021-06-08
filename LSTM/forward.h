bf33cdd15edd6c7925b800aebb25fdfca7eb7dc1
Edvard Ghazaryan <edvard@mini.local>
2021-06-07 23:26:52 -0700

    minor fix

--- LSTM/forward.h
+++
@@ -1,9 +0,0 @@
-#include <ATen/ATen.h>
-#include <vector>
-#include <tuple>
-
-using namespace at;
-
-
-
-std::tuple<Tensor, Tensor>  synthetic_forward (Tensor& ginput_1, std::tuple<Tensor, Tensor> & ghidden_1, Tensor& gwih_1, Tensor& gwhh_1, Tensor& gbih_1, Tensor& gbhh_1);
