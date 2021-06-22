bece103199731b3b1a514b2478fe58224643d470
Ubuntu <ubuntu@ip-172-31-18-150.us-east-2.compute.internal>
2021-06-22 18:52:10 +0000

    fix for LINUX libtorch

--- scripts/get_libtorch.sh
+++ scripts/get_libtorch.sh
@@ -7,8 +7,21 @@ then
     rm -rf ext
     mkdir ext
     cd ext
-    wget https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.7.1.zip
-    echo "Extracting libtorch ..."
-    tar xvzf libtorch-macos-1.7.1.zip
+    if [[ "$OSTYPE" == "linux-gnu"* ]]
+    then
+        # LINUX
+        wget https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-1.7.1%2Bcpu.zip
+        echo "Extracting libtorch ..."
+        unzip libtorch-shared-with-deps-1.7.1+cpu.zip
+    elif [[ "$OSTYPE" == "darwin"* ]]
+    then
+        # Mac OSX
+        wget https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.7.1.zip
+        echo "Extracting libtorch ..."
+        tar xvzf libtorch-macos-1.7.1.zip
+    else
+ 	echo "unknown OS type"
+	exit
+    fi
     cd -
 fi
